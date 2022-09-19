1b618d289e77b1c7665d6caaf9d6c69615038bd8
Ilyas <kz.smile@bk.ru>
2022-09-19 16:43:31 +0600

    first task completed

--- itstep11/js/script.js
+++ itstep11/js/script.js
@@ -1,58 +1,18 @@
 "use strict";
 
 
-// 1. Дана строка. Если в строке больше трех различных символов, то удалить в строке три любых различных символа, иначе добавить в строку в произвольных местах новые элементы так, чтобы количество различных элементов было больше трех. 
-
-let str = 'lorem ipsum dolor sit amet consectetur';
-let arr = str.split(' ');
-let more = Math.ceil(Math.random() * (arr.length - 3));
-let less = Math.ceil(Math.random() * arr.length);
-
-console.log(str);
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr.length > 3) {
-        arr.splice(more, 3);
-    } else if (arr.length < 3) {
-        arr.splice(less, 0, 'one', 'two', 'three', 'four');
-    }
-}
-
-console.log(arr.join(' '));
-
-
-
-
-// 2. Строка состоит из слов, разделенных одним или несколькими пробелами. Поменяйте местами наибольшее по длине слово и наименьшее.
-
-let str = "lorem ipsum dolor sit amet consectetur";
-let arr = str.split(" ");
-
-let min = arr[0];
-let max = arr[0];
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i].length < min.length) {
-    min = arr[i];
-  }else if (arr[i].length > max.length) {
-    max = arr[i];
+//Шахматная доска. Напишите программу, создающую строку, содержащую решётку 8х8, в которой линии разделяются символами новой строки. На каждой позиции либо пробел, либо #. В результате должна получиться шахматная доска.
+let str = '';
+let n = 8;
+for (let i = 0; i < n * n + n - 1; i++) {
+  if (i === 0 || i % 2 === 0) {
+    str += '#';
+  } else {
+    str += ' '; 
+  }
+  if ((str.length + 1) % (n + 1) === 0) {
+    str += '\n';
+    i += 1;
   }
 }
-console.log(min);
-console.log(max);
-
-let minIndex = arr.indexOf(min);
-let maxIndex = arr.indexOf(max);
-console.log(minIndex);
-console.log(maxIndex);
-
-[arr[maxIndex], arr[minIndex]] = [arr[minIndex], arr[maxIndex]];
-console.log(arr.join(' '));
-
-
-
-
-// 3. Строка состоит из слов, разделенных одним или несколькими пробелами. Переставьте слова в алфавитном порядке.
-let str = 'lorem ipsum dolor sit amet consectetur adipisicing elit';
-let arr = str.split(' ');
-let sort = arr.sort();
-console.log(sort.join(' '));
+console.log(str);
